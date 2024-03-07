7f2ca38b3bbfaafb038e28fca66443fa73c002eb
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-07 19:09:22 +0100

    ADD:generatore di numeri randomici 1-100

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ console.log ('ciao' + name + secondname)
 let color = prompt('inserisci il tuo colore preferito') //string - null
 console.log ('il tuo colore preferito è il' + color)
  
-let randomfloat = Math.random() //number
+let randomfloat = Math.floor(Math.random() *100) +1 //number 1 - 100
 
 const passward = name + secondname + color + randomfloat //string
 console.log ('la tua passward è :' + passward)
+
+    
