1be9da5143882b0911c86c3b18775d570ff27bc6
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-07 01:44:25 +0100

    ADD: foglio html + js e inizio consol log di domande

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let name = prompt('inserisci il tuo nome') //string - null
+
+let secondname = prompt('inserisci il tuo cognome') //string - null
+console.log ('ciao' + name + secondname)
+
+let color = prompt('inserisci il tuo colore preferito') //string - null
+console.log ('il tuo colore preferito è il' + color)
